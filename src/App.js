ff6c70f5741f8f6e83e866c172a4bbcd458019fa
Randika Rodrigo <randika.rodrigo@qmatic.com>
2020-01-15 21:32:19 +0530

    Implemented firebase utils with sign up component including ability to authenticated users into firestore database.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
 	constructor(props) {
@@ -19,12 +19,28 @@ class App extends Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-			this.setState({
-				currentUser: user
-			});
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-			console.log(user);
+				userRef.onSnapshot((snapshot) => {
+					this.setState(
+						{
+							currentUser: {
+								id: snapshot.id,
+								...snapshot.data()
+							}
+						},
+						() => {
+							console.log(this.state);
+						}
+					);
+				});
+			} else {
+				this.setState({
+					currentUser: userAuth
+				});
+			}
 		});
 	}
 
